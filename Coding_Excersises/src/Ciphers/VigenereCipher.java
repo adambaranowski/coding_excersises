e01946c86cfb26f529436ec237a313f3e1c79a18
adambaranowski <adamb1066@gmail.com>
2020-03-07 15:44:49 +0100

    decode

--- Coding_Excersises/src/Ciphers/VigenereCipher.java
+++ Coding_Excersises/src/Ciphers/VigenereCipher.java
@@ -26,6 +26,9 @@ public class VigenereCipher {
             if (word.substring(i, i + 1).equals(" ")) {
                 stringBuilder.append(" ");
             } else {
+                if(mainkey.substring(mainKeyIndex, mainKeyIndex + 1).equals(" ")){
+                    mainKeyIndex++;
+                }
                 stringBuilder.append(mainkey.substring(mainKeyIndex, mainKeyIndex + 1));
                 mainKeyIndex++;
             }
@@ -67,8 +70,77 @@ public class VigenereCipher {
         return cryptogram;
     }
 
+
+    public static String decode(String word, String mainkey) {
+
+        List<ArrayList<String>> matrix = new ArrayList<ArrayList<String>>();
+
+        String cryptogram;
+        char c;
+        StringBuilder stringBuilder = new StringBuilder();
+        word = word.toUpperCase();
+        mainkey = mainkey.toUpperCase();
+
+
+        String key;
+
+        int mainKeyIndex = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (mainKeyIndex >= mainkey.length()) {
+                mainKeyIndex = 0;
+            }
+            if (word.substring(i, i + 1).equals(" ")) {
+                stringBuilder.append(" ");
+            } else {
+                if(mainkey.substring(mainKeyIndex, mainKeyIndex + 1).equals(" ")){
+                    mainKeyIndex++;
+                }
+                stringBuilder.append(mainkey.substring(mainKeyIndex, mainKeyIndex + 1));
+                mainKeyIndex++;
+            }
+        }
+
+        key = stringBuilder.toString();
+
+        for (int j = 0; j < 26; j++) {
+            ArrayList<String> row = new ArrayList<String>();
+            int charCounter = j;
+            for (int i = 0; i < 26; i++) {
+
+                c = (char) (charCounter + 65);
+                row.add(String.valueOf(c));
+                charCounter++;
+                if (charCounter == 26) {
+                    charCounter = 0;
+                }
+            }
+            matrix.add(row);
+        }
+
+        int row;
+        int collumn;
+        stringBuilder.delete(0, stringBuilder.length());
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.substring(i, i + 1).equals(" ")) {
+                stringBuilder.append(" ");
+
+            } else {
+                row = matrix.get(0).indexOf(key.substring(i, i + 1));
+                collumn = matrix.get(row).indexOf(word.substring(i, i + 1));
+                stringBuilder.append(matrix.get(0).get(collumn));
+            }
+
+        }
+        cryptogram = stringBuilder.toString();
+        return cryptogram;
+    }
+
+
     public static void main(String[] args) {
-        String d = code("agh to chuje jebac ich w pizde kurwa", "agh");
+        String d = code("super fajna uczelnia agh XD", "science and technology");
+        String c = decode(d, "science and technology" );
         System.out.println(d);
+        System.out.println(c);
     }
 }
