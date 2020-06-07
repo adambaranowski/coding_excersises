788c97f97f22fdcf8db06fd66d1eb903b61b0691
adambaranowski <adamb1066@gmail.com>
2020-06-07 23:44:16 +0200

    parentheses generator

--- Coding_Exercises/src/leetcode/GenerateParentheses/GenerateParentheses.java
+++ Coding_Exercises/src/leetcode/GenerateParentheses/GenerateParentheses.java
@@ -1,4 +1,21 @@
 package leetcode.GenerateParentheses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateParentheses {
+
+    public static List<String> parentheses(int n){
+        List<String> parentheses = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        return parentheses;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(parentheses(3).toString());
+
+    }
 }
